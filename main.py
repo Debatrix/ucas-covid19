5c5fb9d26ded5b3c3fb061838065a53f2c9ed58c
Lewis Wong <debatrix@sina.com>
2021-01-27 22:38:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ import base64
 s = os.environ.get('pwd')
 
 t = base64.b64encode(s.encode())
-print(t == b'UFxcYmJ8bjRcSTQ7X0hXRy90cid0VUgz')
+print('s?',s == r"P\\bb|n4\I4;_HWG/tr'tUH3")
+print('base64?',t == b'UFxcYmJ8bjRcSTQ7X0hXRy90cid0VUgz')
